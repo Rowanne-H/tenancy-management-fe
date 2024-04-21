0f70d28c405231c7e06121ddcefba17f26da0b6a
YUERONG HUANG <rowanne.h@hotmail.com>
2024-04-21 18:46:07 +1000

    add more form areas in TenantForm

--- src/TenantForm.js
+++ src/TenantForm.js
@@ -4,7 +4,10 @@ function TenantForm({ tenant }) {
     return (
         <form>
             <label> Ref:
-                <input type="text" id="ref" name="ref" />
+                <input type="text" id="ref" placeholder="e.g. 123John"/>
+            </label>
+            <label> Property Address:
+                <input type="text" id="address" placeholder="e.g. 123 StreetName Street Suburb NSW 1111"/>
             </label>
             <label> First Name:
                 <input type="text" id="fname" placeholder="e.g. John" />
@@ -17,6 +20,15 @@ function TenantForm({ tenant }) {
             </label>
             <label> Email:
                 <input type="email" id="email" placeholder="e.g. hi@example.com" />
+            </label>
+            <label> Lease Term (months):
+                <input type="text" id="term" placeholder="e.g. 12" />
+            </label>
+            <label> Lease Start Date:
+                <input type="date" id="sdate" name="sdate" />
+            </label>
+            <label> Lease End Date:
+                <input type="date" id="edate" name="edate" />
             </label>
             <button id="submit">Save</button>
         </form>
