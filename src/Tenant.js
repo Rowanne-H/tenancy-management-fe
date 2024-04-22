1a7f297c7594a65dd386e6b57cd280ef3bb89270
YUERONG HUANG <rowanne.h@hotmail.com>
2024-04-22 19:40:41 +1000

    create function to delete a tenant in json-server when delete button is clicked

--- src/Tenant.js
+++ src/Tenant.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 function Tenant({ tenant }) {
     const {id, ref, property, fname, lname, mobile, email, term, sdate, edate, note} = tenant
+    function handleDeleteClick() {
+        fetch(`https://tenancy-management-be.onrender.com/tenants/${id}`, {
+            method: "DELETE",
+        })
+            .then(r => r.json())
+            .then(() => console.log("deleted!"));
+    }
     return (
         <tr>
             <td>{ref}</td>
@@ -16,7 +23,7 @@ function Tenant({ tenant }) {
             <td>
                 <button>View</button>
                 <button>Edit</button>
-                <button>Delete</button>
+                <button onClick={handleDeleteClick}>Delete</button>
             </td>
             
         </tr>
