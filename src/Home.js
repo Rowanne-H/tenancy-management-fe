d2f7cb6167b4b204997dc4e70f89deffa3283dc8
YUERONG HUANG <rowanne.h@hotmail.com>
2024-04-23 21:57:04 +1000

    change home headings

--- src/Home.js
+++ src/Home.js
@@ -3,16 +3,16 @@ import React from 'react';
 function Home() {
     return (
         <div id="description">
-            <h3>Tenancy Management System</h3>
-            <h4>You can use Tenancy Management System to keep and update tenants record</h4>
-            <h5> "Tenants" page</h5>
+            <h1>Tenancy Management System</h1>
+            <h2>You can use Tenancy Management System to keep and update tenants record</h2>
+            <h3> "Tenants" page</h3>
             <p>
                 You will see a list for all tenants with options for you to view selected tenant's details, edit selected tenant and delete selected tenant. 
                 When "view" is clicked, you will see a new page for this selected tenant's detials.
                 When "Edit" is clicked, you will see an edit form with pre-filled selected tenant's details in input areas which you can edit. Once "Save" button is clicked, all changes will be saved into the record.
                 When "Delete" is clicked, this selected tenant will be deleted from the record.
             </p>
-            <h5>"Add New Tenant" page</h5>
+            <h3>"Add New Tenant" page</h3>
             <p>You will see a form which you can key in new tenant's information and add this new tenant to the record.</p>
         </div>
     )
