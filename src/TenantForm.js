5065bccb8496db044fa047b4c0df2923e5b5d945
YUERONG HUANG <rowanne.h@hotmail.com>
2024-04-23 22:24:10 +1000

    delete useEffect import in tenantForm as it is not used

--- src/TenantForm.js
+++ src/TenantForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 function TenantForm({ onAddNewTenant }) {
     let form = document.querySelector('form');
