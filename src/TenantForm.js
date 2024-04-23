e8eaa86965142472d1ae5e6525682440f5ccb0c0
YUERONG HUANG <rowanne.h@hotmail.com>
2024-04-23 21:03:57 +1000

    create a search input and Inactive checkbox in Tenants component to filter tenants

--- src/TenantForm.js
+++ src/TenantForm.js
@@ -48,10 +48,10 @@ function TenantForm({ onAddNewTenant }) {
                 <input type="tel" id="mobile" pattern="04[0-9]{8}" placeholder="e.g. 0400111000" onChange={handleChange} />
             </label>
             <label> Email:
-                <input type="email" id="email" placeholder="e.g. hi@example.com" onChange={handleChange} />
+                <input type="email" id="email" name="email" placeholder="e.g. hi@example.com" onChange={handleChange} />
             </label>
             <label> Lease Term (months):
-                <input type="text" id="term" placeholder="e.g. 12" onChange={handleChange} />
+                <input type="text" id="term" name="term" placeholder="e.g. 12" onChange={handleChange} />
             </label>
             <label> Lease Start Date:
                 <input type="date" id="sdate" name="sdate" onChange={handleChange} />
--- src/Tenants.js
+++ src/Tenants.js
@@ -3,24 +3,28 @@ import Tenant from './Tenant';
 
 function Tenants({ tenants, deleteTenant }) {
     return (
-        <table>
-            <tbody>
-                <tr>
-                    <th>Ref</th>
-                    <th>Property</th>
-                    <th>Name</th>
-                    <th>Mobile</th>
-                    <th>Email</th>
-                    <th>Lease Term</th>
-                    <th>Start Date</th>
-                    <th>End Date</th>
-                    <th>Note</th>
-                    <th>Status</th>
-                    <th>More</th>
-                </tr>
-                {tenants.map(tenant => <Tenant key={tenant.id} tenant={tenant} onDeleteTenant={deleteTenant} />)}
-            </tbody>
-        </table>
+        <div>
+            <input type="text" id="search" name="search" placeholder="search..." />
+            <span>Inactive</span><input type="checkbox" value="Inactive" />
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Ref</th>
+                        <th>Property</th>
+                        <th>Name</th>
+                        <th>Mobile</th>
+                        <th>Email</th>
+                        <th>Lease Term</th>
+                        <th>Start Date</th>
+                        <th>End Date</th>
+                        <th>Note</th>
+                        <th>Status</th>
+                        <th>More</th>
+                    </tr>
+                    {tenants.map(tenant => <Tenant key={tenant.id} tenant={tenant} onDeleteTenant={deleteTenant} />)}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
