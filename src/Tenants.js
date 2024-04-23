8393fc9665300b50ae71feb7b8ba983c300ad10a
YUERONG HUANG <rowanne.h@hotmail.com>
2024-04-23 22:15:37 +1000

    make Tenants component code more readable

--- src/Tenants.js
+++ src/Tenants.js
@@ -5,26 +5,37 @@ function Tenants({ tenants, deleteTenant }) {
     const [search, setSearch] = useState('');
     const [showActiveTenants, setShowActiveTenants] = useState(false);
     const filteredTenants = tenants
-    .filter(tenant => {
-        if (showActiveTenants) {
-            return tenant.status === 'Active'
-        } else {
-            return true
-        }
-    })
-    .filter(tenant => 
-        tenant.ref.toLowerCase().includes(search)
-        || tenant.property.toLowerCase().includes(search) 
-        || tenant.fname.toLowerCase().includes(search)
-        || tenant.lname.toLowerCase().includes(search)
-        || tenant.mobile.toLowerCase().includes(search)
-        || tenant.email.toLowerCase().includes(search)
-    )    
-    
+        .filter(tenant => {
+            if (showActiveTenants) {
+                return tenant.status === 'Active'
+            } else {
+                return true
+            }
+        })
+        .filter(tenant =>
+            tenant.ref.toLowerCase().includes(search)
+            || tenant.property.toLowerCase().includes(search)
+            || tenant.fname.toLowerCase().includes(search)
+            || tenant.lname.toLowerCase().includes(search)
+            || tenant.mobile.toLowerCase().includes(search)
+            || tenant.email.toLowerCase().includes(search)
+        )
+
     return (
         <div>
-            <input type="text" id="search" name="search" placeholder="search..." onChange={(e) => setSearch(e.target.value.toLowerCase())}/>
-            <span>Active Tenants Only</span><input type="checkbox" checked={showActiveTenants} onChange={() => setShowActiveTenants(!showActiveTenants)}/>
+            <input
+                type="text"
+                id="search"
+                name="search"
+                placeholder="search..."
+                onChange={(e) => setSearch(e.target.value.toLowerCase())}
+            />
+            <span>Active Tenants Only</span>
+            <input
+                type="checkbox"
+                checked={showActiveTenants}
+                onChange={() => setShowActiveTenants(!showActiveTenants)}
+            />
             <table>
                 <tbody>
                     <tr>
