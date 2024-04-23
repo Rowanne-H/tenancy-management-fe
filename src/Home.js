67566570839fdbe0a966c2df47bff7455916ee23
YUERONG HUANG <rowanne.h@hotmail.com>
2024-04-23 22:10:56 +1000

    add desciption for search and checkbox in Home component

--- src/Home.js
+++ src/Home.js
@@ -6,12 +6,12 @@ function Home() {
             <h1>Tenancy Management System</h1>
             <h2>You can use Tenancy Management System to keep and update tenants record</h2>
             <h3> "Tenants" page</h3>
-            <p>
-                You will see a list for all tenants with options for you to view selected tenant's details, edit selected tenant and delete selected tenant. 
-                When "view" is clicked, you will see a new page for this selected tenant's detials.
-                When "Edit" is clicked, you will see an edit form with pre-filled selected tenant's details in input areas which you can edit. Once "Save" button is clicked, all changes will be saved into the record.
-                When "Delete" is clicked, this selected tenant will be deleted from the record.
-            </p>
+            <p>You will see a list for all tenants with options for you to view selected tenant's details, edit selected tenant and delete selected tenant. </p>
+            <p>Search bar allow you to filter tenants by their reference, property, firt name, last name, mobile and email</p>
+            <p>"Active Tenants Only" checkbox allow you to show the active tenants only. </p>
+            <p>When "view" is clicked, you will see a new page for this selected tenant's detials.</p>
+            <p>When "Edit" is clicked, you will see an edit form with pre-filled selected tenant's details in input areas which you can edit. Once "Save" button is clicked, all changes will be saved into the record.</p>
+            <p> When "Delete" is clicked, this selected tenant will be deleted from the record.</p>
             <h3>"Add New Tenant" page</h3>
             <p>You will see a form which you can key in new tenant's information and add this new tenant to the record.</p>
         </div>
