21b880c235f104569e696e6b41822ac1bd952b67
YUERONG HUANG <rowanne.h@hotmail.com>
2024-04-29 18:09:47 +1000

    update EditTenantForm to make sure all esstential information has been updated before saving the tenant

--- src/EditTenantForm.js
+++ src/EditTenantForm.js
@@ -43,25 +43,25 @@ function EditTenantForm({ onUpdateTenant }) {
     return (
         <form onSubmit={handleSubmit}>
             <label> Ref:
-                <input type="text" name="ref" placeholder="e.g. 123John" onChange={handleChange} value={tenant.ref} />
+                <input type="text" name="ref" placeholder="e.g. 123John" onChange={handleChange} value={tenant.ref} required/>
             </label>
             <label> Property:
-                <input type="text" name="property" placeholder="e.g. 123 StreetName Street Suburb NSW 1111" onChange={handleChange} value={tenant.property} />
+                <input type="text" name="property" placeholder="e.g. 123 StreetName Street Suburb NSW 1111" onChange={handleChange} value={tenant.property} required/>
             </label>
             <label> First Name:
-                <input type="text" name="fname" placeholder="e.g. John" onChange={handleChange} value={tenant.fname} />
+                <input type="text" name="fname" placeholder="e.g. John" onChange={handleChange} value={tenant.fname} required/>
             </label>
             <label> Last Name:
-                <input type="text" name="lname" placeholder="e.g. Doe" onChange={handleChange} value={tenant.lname} />
+                <input type="text" name="lname" placeholder="e.g. Doe" onChange={handleChange} value={tenant.lname} required/>
             </label>
             <label> Mobile:
-                <input type="tel" name="mobile" pattern="04[0-9]{8}" placeholder="e.g. 0400111000" onChange={handleChange} value={tenant.mobile} />
+                <input type="tel" name="mobile" pattern="04[0-9]{8}" placeholder="e.g. 0400111000" onChange={handleChange} value={tenant.mobile} required/>
             </label>
             <label> Email:
-                <input type="email" name="email" placeholder="e.g. hi@example.com" onChange={handleChange} value={tenant.email} />
+                <input type="email" name="email" placeholder="e.g. hi@example.com" onChange={handleChange} value={tenant.email} required/>
             </label>
             <label> Lease Term (months):
-                <input type="text" name="term" placeholder="e.g. 12" onChange={handleChange} value={tenant.term} />
+                <input type="text" name="term" placeholder="e.g. 12" onChange={handleChange} value={tenant.term} required/>
             </label>
             <label> Lease Start Date: {tenant.sdate}
                 <input type="date" name="sdate" onChange={handleChange} />
